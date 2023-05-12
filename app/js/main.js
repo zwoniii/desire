03deb08f0530aa178879adf39374f65ac740611e
irazv <zwoniii@ya.ru>
2023-05-12 14:38:53 +0300

    contacts adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,7 @@ $(function () {
         $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
     }
 
-    
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -27,7 +27,51 @@ $(function () {
         dots: true,
         arrows: false,
         slidesToShow: 10,
-        slidesToScroll: 10
+        slidesToScroll: 10,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
     $('.article-slider__box').slick({
