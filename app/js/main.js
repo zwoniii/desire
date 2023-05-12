da34e126e326d960554c4f04ffbfa59dc4b98270
irazv <zwoniii@ya.ru>
2023-05-12 13:32:08 +0300

    works-path adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,10 @@ $(function () {
         $('.menu').toggleClass('menu--open');
     });
 
+    if ($(window).width() < 651) {
+        $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+    }
+
     
     $('.top__slider').slick({
         dots: true,
